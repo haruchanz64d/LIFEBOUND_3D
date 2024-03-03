d81461d3a7ce6dfa86767e8e5ddc9329aeb2507d
Haru <radam.aronjake@gmail.com>
2024-03-03 10:20:54 +0800

    Update

--- Assets/Scripts/Gameplay/LBCharacterSelect.cs
+++ Assets/Scripts/Gameplay/LBCharacterSelect.cs
@@ -1,78 +1,17 @@
 using UnityEngine;
-using TMPro;
-using Mirror;
-using System.Collections.Generic;
 
-public class LBCharacterSelect : NetworkBehaviour
+public class LBCharacterSelect : MonoBehaviour
 {
     [SerializeField] private GameObject characterSelectedDisplay = default;
-    [SerializeField] private Transform characterPreviewParent = default;
-    [SerializeField] private TMP_Text characterSelectName = default;
+    [SerializeField] private Transform[] characterPreviewParents = default;
+
     [SerializeField] private float turnSpeed = 90f;
-    [SerializeField] private LBCharacter[] lBCharacters = default;
-
-    private int currentIndex = 0;
-    private List<GameObject> characterInstances = new List<GameObject>();
-
-    public override void OnStartClient()
-    {
-        if (characterPreviewParent.childCount == 0)
-        {
-            foreach (var character in lBCharacters)
-            {
-                GameObject characterInstance =
-                Instantiate(character.PreviewPrefab, characterPreviewParent);
-
-                characterInstance.SetActive(false);
-
-                characterInstances.Add(characterInstance);
-            }
-        }
-
-        characterInstances[currentIndex].SetActive(true);
-        characterSelectName.text = lBCharacters[currentIndex].CharacterName;
-
-        characterSelectedDisplay.SetActive(true);
-    }
 
     private void Update()
     {
-        characterPreviewParent.RotateAround(characterPreviewParent.position, characterPreviewParent.up, turnSpeed * Time.deltaTime);
-    }
-
-    public void SelectCharacter()
-    {
-        CmdSelect(currentIndex);
-        characterSelectedDisplay.SetActive(false);
-    }
-
-    public void CmdSelect(int currentIndex, NetworkConnectionToClient sender = null)
-    {
-        GameObject characterInstance = Instantiate(lBCharacters[currentIndex].GameplayPrefab);
-        NetworkServer.Spawn(characterInstance, sender);
-    }
-
-    public void SelectLeft()
-    {
-        characterInstances[currentIndex].SetActive(false);
-
-        currentIndex--;
-        if (currentIndex < 0)
+        for(int x = 0; x < characterPreviewParents.Length; x++)
         {
-            currentIndex += characterInstances.Count;
+            characterPreviewParents[x].RotateAround(characterPreviewParents[x].position, characterPreviewParents[x].up, turnSpeed * Time.deltaTime);
         }
-
-        characterInstances[currentIndex].SetActive(true);
-        characterSelectName.text = lBCharacters[currentIndex].CharacterName;
-    }
-
-    public void SelectRight()
-    {
-        characterInstances[currentIndex].SetActive(false);
-
-        currentIndex = (currentIndex + 1) % characterInstances.Count;
-
-        characterInstances[currentIndex].SetActive(true);
-        characterSelectName.text = lBCharacters[currentIndex].CharacterName;
     }
 }
--- Assets/Scripts/Network/LBNetworkManager.cs
+++ Assets/Scripts/Network/LBNetworkManager.cs
@@ -1,6 +1,24 @@
 using Mirror;
+using TMPro;
 using UnityEngine;
-public class LBNetworkManager : NetworkManager
+public class LBNetworkManager : MonoBehaviour
 {
+    private NetworkManager manager;
+    [SerializeField] private TMP_InputField inputField;
+    [SerializeField] private GameObject solGameplayPrefab; // HOST
+    [SerializeField] private GameObject lunaGameplayPrefab; // CLIENT
+    private void Awake()
+    {
+        manager = GetComponent<NetworkManager>();
+    }
 
+    // Server + Client
+    public void PlayAsSol(){
+        manager.StartHost();
+    }
+
+    // Client + IP (Port)
+    public void PlayAsLuna(){
+        
+    }
 }
