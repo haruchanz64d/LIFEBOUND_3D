9e44d7058ddb39b3a0c04e5557779f1789cc9400
Haru <radam.aronjake@gmail.com>
2024-03-05 12:29:00 +0800

    Added script to automate building Linux server

---
+++ build.sh
@@ -0,0 +1,3 @@
+echo "Starting build..."
+"C:\Program Files\Unity\Hub\Editor\2021.3.34f1\Editor\Unity.exe" -quit -batchmode -projectPath "D:\Projects\LIFEBOUND_3D" -executeMethod LBBuild.BuildLinuxServer
+echo "Finised build!"
