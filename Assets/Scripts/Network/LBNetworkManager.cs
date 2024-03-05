e7282ff9cd56a376b13b28416e726bc0f3642159
Haru <radam.aronjake@gmail.com>
2024-03-05 12:28:41 +0800

    Updated the network manager to just spawn the players

--- Assets/Scripts/Network/LBNetworkManager.cs
+++ Assets/Scripts/Network/LBNetworkManager.cs
@@ -4,21 +4,26 @@ using UnityEngine;
 public class LBNetworkManager : MonoBehaviour
 {
     private NetworkManager manager;
+    [SerializeField] private Canvas selectionCanvas;
     [SerializeField] private TMP_InputField inputField;
     [SerializeField] private GameObject solGameplayPrefab; // HOST
     [SerializeField] private GameObject lunaGameplayPrefab; // CLIENT
-    private void Awake()
-    {
+
+    private void Awake(){
         manager = GetComponent<NetworkManager>();
     }
-
+    
     // Server + Client
     public void PlayAsSol(){
-        manager.StartHost();
+        GameObject solPlayer = Instantiate(solGameplayPrefab);
+        NetworkServer.Spawn(solPlayer);
+        selectionCanvas.enabled = false;
     }
 
     // Client + IP (Port)
     public void PlayAsLuna(){
-        
+        GameObject lunaPlayer = Instantiate(lunaGameplayPrefab);   
+        NetworkServer.Spawn(lunaPlayer);
+        selectionCanvas.enabled = false;
     }
 }
