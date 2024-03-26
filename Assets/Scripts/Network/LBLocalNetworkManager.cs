9e47a6a67fee17c02c2c5c2a4e1311b6d9c4e35a
Haru <radam.aronjake@gmail.com>
2024-03-26 19:17:24 +0800

    Update

--- Assets/Scripts/Network/LBLocalNetworkManager.cs
+++ Assets/Scripts/Network/LBLocalNetworkManager.cs
@@ -3,13 +3,21 @@ using UnityEngine;
 
 public class LBLocalNetworkManager : NetworkBehaviour
 {
+    public GameObject canvas;
     public void StartHost()
     {
         NetworkManager.StartHost();
+        DestroyCanvas();
     }
 
     public void StartClient()
     {
         NetworkManager.StartClient();
+        DestroyCanvas();
+    }
+
+    public void DestroyCanvas()
+    {
+        Destroy(canvas);
     }
 }
